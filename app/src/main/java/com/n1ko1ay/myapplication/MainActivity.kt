51129a65b24fd99eb892f930397013643c52c569
N1ko1ay <gastaclaus63@gmail.com>
2021-11-28 20:01:37 +0400

    added app

--- app/src/main/java/com/n1ko1ay/myapplication/MainActivity.kt
+++ app/src/main/java/com/n1ko1ay/myapplication/MainActivity.kt
@@ -12,32 +12,32 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Log.d(TAG, "onCreate")
+        Log.d(TAG, "Зовет меня взглядом и криком своим")
     }
 
     override fun onStart() {
         super.onStart()
-        Log.d(TAG, "onStart")
+        Log.d(TAG, "И вымолвить хочет: «Давай улетим!")
     }
 
     override fun onResume() {
         super.onResume()
-        Log.d(TAG, "onResume")
+        Log.d(TAG, "Мы вольные птицы; пора, брат, пора!")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.d(TAG, "onPause")
+        Log.d(TAG, "Туда, где за тучей белеет гора,")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.d(TAG, "onStop")
+        Log.d(TAG, "Туда, где синеют морские края,")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.d(TAG, "onDestroy")
+        Log.d(TAG, "Туда, где гуляем лишь ветер... да я!")
     }
 }
 
