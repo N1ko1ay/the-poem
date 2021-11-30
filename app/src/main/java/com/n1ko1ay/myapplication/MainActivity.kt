44e0c5aad31a6fd25aff959edf81f592664b7517
N1ko1ay <gastaclaus63@gmail.com>
2021-11-30 20:13:54 +0400

    4.2

--- app/src/main/java/com/n1ko1ay/myapplication/MainActivity.kt
+++ app/src/main/java/com/n1ko1ay/myapplication/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.n1ko1ay.myapplication
 
+import android.content.Intent
 import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 const val TAG= "MainActivity"
 
+private const val HELLO_KEY = "hello"
+
 class MainActivity : AppCompatActivity() {
+
+    lateinit var nextActivity: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextActivity = findViewById(R.id.next_activity_button)
+
+        nextActivity.setOnClickListener {
+            val secondActivityIntent:Intent = Intent(this, SecondActivity::class.java)
+
+            secondActivityIntent.putExtra(HELLO_KEY, "Hello from MainActivity")
+
+            startActivity(secondActivityIntent)
+        }
 
         Log.d(TAG, "Зовет меня взглядом и криком своим")
     }
